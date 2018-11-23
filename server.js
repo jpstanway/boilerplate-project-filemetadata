c134b510e6967968a663669f1c98e75663208584
Jordan S <jpstanway@gmail.com>
2018-11-23 14:59:14 -0800

    upload file to memory storage and add error handling

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 
 var express = require('express');
 var cors = require('cors');
-var app = express();
+
 
 // require and use "multer"...
 var multer = require('multer');
-app.use(multer({dest: 'uploads/'}));
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage}).single('upfile');
+
+var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +20,19 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+// file upload
+app.post('/api/fileanalyse', (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      res.send('There was an error with Multer when uploading');
+    } else if (err) {
+      res.send('Unknown error occured when uploading');
+    }
+
+    res.send({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
